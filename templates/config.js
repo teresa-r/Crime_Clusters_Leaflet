e0fc73e93703fec12d96f4417d0872b020e159e2
teresa <truan1130@gmail.com>
2019-10-31 12:26:40 -0700

    Update

--- templates/config.js
+++
@@ -1,3 +0,0 @@
-// API key
-// const API_KEY = "pk.eyJ1IjoidHJ1YW4xMTMwIiwiYSI6ImNrMGVpdThiOTBpZzYzZW1ta3IxMXd2eDkifQ.Cwp96dF7UwebzhpeEKeSFA";
-const API_KEY = "pk.eyJ1IjoidHJ1YW4xMTMwIiwiYSI6ImNrMGVpdThiOTBpZzYzZW1ta3IxMXd2eDkifQ.Cwp96dF7UwebzhpeEKeSFA"
